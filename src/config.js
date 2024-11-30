6d381ecfb705d018321a2985c35e5d5c90fb8334
pmanonline <petersonzoconis@gmail.com>
2024-11-30 14:09:01 +0100

    updated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const backendURL = "https://autographbackend.vercel.app";
+const backendURL = "https://autographbackend.vercel.app/";
 // const backendURL = "https://autoapi.eroot.ng";
 // const backendURL = "http://localhost:3001";
 
