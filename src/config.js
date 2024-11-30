be373940a0f09a6a053652500e791f6739693ede
pmanonline <petersonzoconis@gmail.com>
2024-11-30 14:05:38 +0100

    updated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const backendURL = "https://autoapi.eroot.ng";
+const backendURL = "https://autographbackend.vercel.app";
+// const backendURL = "https://autoapi.eroot.ng";
 // const backendURL = "http://localhost:3001";
 
 export default backendURL;
